0e1e823e102f95137bbfec1317f425f9dea38c1e
Gaik Artur <gaik.artur@yandex.ru>
2023-10-22 05:35:22 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from './components/Content/Home/Home';
 import About from './components/Content/About/About';
 import Classifier from './components/Content/Classifier/Classifier';
 import Contact from './components/Content/Contact/Contact';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/classifier" element={<Classifier />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/classifier" element={<Classifier/>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
